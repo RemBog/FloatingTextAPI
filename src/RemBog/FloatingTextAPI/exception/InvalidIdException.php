9b49a06cda4759b78dc2ffd789abf0891382ce19
RemBog <87894259+RemBog@users.noreply.github.com>
2022-08-16 11:39:56 +0200

    Add files via upload

---
+++ src/RemBog/FloatingTextAPI/exception/InvalidIdException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RemBog\FloatingTextAPI\exception;
+
+use pocketmine\InvalidArgumentException;
+
+final class InvalidIdException extends \InvalidArgumentException{ 
+
+}
