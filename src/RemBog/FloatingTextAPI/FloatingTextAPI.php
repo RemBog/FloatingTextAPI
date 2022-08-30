a736ab03d4d86fa7bbf311f98ea1d7837471e951
RemBog <87894259+RemBog@users.noreply.github.com>
2022-08-30 19:02:15 +0200

    Update in progress...
    Not finished

--- src/RemBog/FloatingTextAPI/FloatingTextAPI.php
+++ src/RemBog/FloatingTextAPI/FloatingTextAPI.php
@@ -8,15 +8,22 @@ use pocketmine\nbt\tag\CompoundTag;
 
 class FloatingTextAPI
 {
-    public static function spawnFloatingText(int $id): void
+    public static function appearFloatingText(int $id): void
     {
         $ft = FloatingTextMain::$list[$id];
         
         $pos = $ft->getPosition();
         $loc = new Location($pos->x, $pos->y, $pos->z, $pos->getWorld(), 0, 0);
         
-	    $entity = new FloatingTextEntity($loc, $id);
+	$entity = new FloatingTextEntity($loc, $id);
 	    
         $entity->spawnToAll();
+    }
+    
+    public static function disappearFloatingText(int $id): void
+    {
+        $ft = self::getFloatingText($id);
+        
+        $ft->();
     }
 }
